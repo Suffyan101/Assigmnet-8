596d4b4637f33c2d2b03f70ea3d17ed38d955cfa
Suffyan101 <suffyan.r01@gmail.com>
2022-06-23 00:42:50 +0500

    Assignment 8

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// Hate and Tale Game
+
+// var a  = prompt("Hate or tale")
+// var b = a.toLowerCase()
+// var c= Math.floor(Math.random()*2)
+
+// if(b === "hate" && c === 0){
+//     alert("Win")
+// }
+// else if(b === "tale" && c === 1){
+//     alert("Win")
+// }
+// else{
+//     alert("Try again")
+// }
+
+// Dies Game
+
+// alert("Ready")
+// var c= Math.floor(Math.random()*6+1)
+// document.write("Dies num is now is:" +c)
